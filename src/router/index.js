4a79483507c39059f130a1bb9d950d1c8bf2b698
Enrico Signori <enricos@wedoc.com.br>
2021-12-25 21:38:46 -0300

    Added pages base and router settings

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import Portfolio from '@/views/Portfolio.vue';
+import KoreTeam from '@/views/KoreTeam.vue';
+import KorePartner from '@/views/KorePartner.vue';
+import Compliance from '@/views/Compliance.vue';
+import KoreNode from '@/views/KoreNode.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: Portfolio,
+  },
+  {
+    path: '/kore-team',
+    name: 'KoreTeam',
+    component: KoreTeam,
+  },
+  {
+    path: '/kore-partner',
+    name: 'KorePartner',
+    component: KorePartner,
+  },
+  {
+    path: '/compliance',
+    name: 'Compliance',
+    component: Compliance,
+  },
+  {
+    path: '/kore-node',
+    name: 'KoreNode',
+    component: KoreNode,
   },
 ];
 
