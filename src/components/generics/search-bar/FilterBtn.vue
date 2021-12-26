64dd0921b049373e8a727f5a49f5498eb8fd819a
Enrico Signori <enricos@wedoc.com.br>
2021-12-25 21:42:23 -0300

    Created Searchbar Components

---
+++ src/components/generics/search-bar/FilterBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <PrimaryOutlineBtn>
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      class="mr-2"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 11.5H17V13.5H7V11.5ZM4 7.5H20V9.5H4V7.5ZM10 15.5H14V17.5H10V15.5Z"
+      />
+    </svg>
+    Filter
+  </PrimaryOutlineBtn>
+</template>
+
+<script>
+import PrimaryOutlineBtn from '@/components/generics/buttons/PrimaryOutlineBtn.vue';
+
+export default {
+  name: 'FilterBtn',
+  components: {
+    PrimaryOutlineBtn,
+  },
+};
+</script>
---
+++ src/components/generics/search-bar/SearchBarContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full flex flex-row items-center justify-between pt-8 px-8">
+    <SearchBar />
+    <FilterBtn />
+  </div>
+</template>
+
+<script>
+import SearchBar from './SearchBar.vue';
+import FilterBtn from './FilterBtn.vue';
+
+export default {
+  name: 'SearchBarContainer',
+  components: {
+    SearchBar,
+    FilterBtn,
+  },
+};
+</script>
+
+<style></style>
