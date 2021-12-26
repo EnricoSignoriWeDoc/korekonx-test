b3e2bcca2a4112955a35be6ea359706a5cc455b8
Enrico Signori <enricos@wedoc.com.br>
2021-12-25 21:43:01 -0300

    Final version of Home Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="">
-
+  <div class="lg:ml-90 py-4 px-22 h-full grow">
+    <div
+      class="container max-w-fit mx-auto flex flex-col justify-center items-center bg-white rounded-2xl default-shadow"
+    >
+      <HomeTopHeader />
+      <TabsNavigation />
+      <SearchBarContainer />
+      <ListContainer />
+      <HomeBottomPagination />
+    </div>
   </div>
 </template>
 
 <script>
+import HomeTopHeader from '@/components/home-page/HomeTopHeader.vue';
+import TabsNavigation from '@/components/generics/tabs/TabsNavigation.vue';
+import SearchBarContainer from '@/components/generics/search-bar/SearchBarContainer.vue';
+import ListContainer from '@/components/generics/lists/ListContainer.vue';
+import HomeBottomPagination from '@/components/home-page/HomeBottomPagination.vue';
+
 export default {
-  name: "Home",
+  name: 'Home',
+  components: {
+    HomeTopHeader,
+    TabsNavigation,
+    SearchBarContainer,
+    ListContainer,
+    HomeBottomPagination,
+  },
 };
 </script>
